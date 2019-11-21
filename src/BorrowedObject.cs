bb3aa4b6c27a94e2c62fd9e10d07cf3d518c43b8
Victor Milovanov <lost@losttech.software>
2019-11-21 15:20:01 -0800

    added some contracts to BorrowedObject

--- src/BorrowedObject.cs
+++ src/BorrowedObject.cs
@@ -1,7 +1,9 @@
 ﻿namespace Python.Runtime.Native {
     using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Diagnostics.Contracts;
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design",
+    [SuppressMessage("Design",
         "CA1066:Type {0} should implement IEquatable<T> because it overrides Equals",
         Justification = "ref structs can not implement interfaces, as they are not boxable")]
     public ref struct BorrowedObject
@@ -12,16 +14,24 @@
             if (handle == IntPtr.Zero) throw new ArgumentNullException(nameof(handle));
             this.handle = handle;
         }
-
+        [Pure]
         public bool Equals(BorrowedObject other) => this.handle == other.handle;
-        public override bool Equals(object obj) => false;
-
+        [Pure]
         public override int GetHashCode() => this.handle.GetHashCode();
-
+        [Pure]
         public static bool operator ==(BorrowedObject left, BorrowedObject right)
             => left.Equals(right);
-
+        [Pure]
         public static bool operator !=(BorrowedObject left, BorrowedObject right)
             => !(left == right);
+
+        // ref struct can never be boxed, so it is impossible to call this via base class (object)
+#pragma warning disable CS0809 // Obsolete member overrides non-obsolete member
+        [Obsolete(message: "This method should never be called, as BorrowedObject can not be boxed", error: true)]
+        [SuppressMessage(category: "Design",
+            checkId: "CA1065:Do not raise exceptions in unexpected locations",
+            Justification = "This method should never be called, which is indicated by Obsolete attribute")]
+        public override bool Equals(object obj) => throw new InvalidOperationException();
+#pragma warning restore CS0809 // Obsolete member overrides non-obsolete member
     }
 }
